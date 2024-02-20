ff38e2e8cdbb38c697a569c424f49fac79a41be5
syedubaid <syedabuubaid@gmail.com>
2024-02-21 02:19:46 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,24 @@
+function createPlayer(name, symbol) {
+  return {
+    name: name,
+    symbol: symbol,
+  };
+}
+let emptygrid=[
+  [0,0,5],
+  [0,0,0]
+  [0,0,0]
+]
+
+let gameboard={
+  grid:emptygrid
+}
+
+document.querySelectorAll("cell")
+updategameBoard={
+
+
+
+}
+
+console.log(gameboard.grid[0][2]) 
